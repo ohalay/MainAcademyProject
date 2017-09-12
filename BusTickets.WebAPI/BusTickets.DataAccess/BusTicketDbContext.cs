e6dc1903edfd36d9c05b06e3dfba0fa639376ecc
ohalay <olehhalay@ukr.net>
2017-09-12 23:45:12 +0300

    -create DataAccess project and add StyleCop

---
+++ BusTickets.WebAPI/BusTickets.DataAccess/BusTicketDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BusTickets.DataAccess
+{
+    public class BusTicketDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = @"Server=.;Database=BusTicket;Trusted_Connection=True;";
+            var retryLogicCount = 3;
+
+            optionsBuilder.UseSqlServer(connection, opton => opton.EnableRetryOnFailure(retryLogicCount));
+        }
+    }
+}

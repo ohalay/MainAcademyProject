fa1d9edf70bf1a185b5c722d83bfe566e5c6f334
mykolanazaruk1992 <mykolanazaruk1992@gmail.com>
2017-09-14 17:46:59 +0300

    Create Tables

--- BusTickets.WebAPI/BusTickets.DataAccess/BusTicketDbContext.cs
+++ BusTickets.WebAPI/BusTickets.DataAccess/BusTicketDbContext.cs
@@ -4,6 +4,16 @@ namespace BusTickets.DataAccess
 {
     public class BusTicketDbContext : DbContext
     {
+        public virtual DbSet<CitiesNearby> CitiesNearbys { get; set; }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        public virtual DbSet<BusStop> BusStops { get; set; }
+
+        public virtual DbSet<BusStation> BusStations { get; set; }
+
+        public virtual DbSet<Journey> Journeys { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connection = @"Server=.;Database=BusTicket;Trusted_Connection=True;";
---
+++ BusTickets.WebAPI/BusTickets.DataAccess/Tables/BusStop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusTickets.DataAccess
+{
+    public class BusStop
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("ID")]
+        public int JourneyID { get; set; }
+
+        [ForeignKey("City")]
+        public int CityID { get; set; }
+
+        [NotMapped]
+        public int Time { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
+    }
+}

029b51d3dd946edd036ef081cd676368e61dee2e
SProtsiv <31449158+SProtsiv@users.noreply.github.com>
2017-09-20 15:43:10 +0300

    My Tables

---
+++ BusTickets.WebAPI/BusTickets.DataAccess/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusTickets.DataAccess
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int Number_Of_Seats { get; set; }
+
+        public int DriverID { get; set; }
+
+        public int Opinion { get; set; }
+
+        [MaxLength(50)]
+        public string Explanation { get; set; }
+
+        [ForeignKey(nameof(DriverID))]
+        public Driver Driver { get; set; }
+    }
+}

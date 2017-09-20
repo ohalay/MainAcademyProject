5fc7c1143796f9c67fc8e0b08e3c1cb02bffd39e
SProtsiv <31449158+SProtsiv@users.noreply.github.com>
2017-09-20 15:43:10 +0300

    My Tables

---
+++ BusTickets.WebAPI/BusTickets.DataAccess/Models/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusTickets.DataAccess
+{
+    public class Driver
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [MaxLength(20)]
+        public string Phone_Number { get; set; }
+
+        [MaxLength(40)]
+        public string Address { get; set; }
+    }
+}

7e305e8e7b9e73e8f85150d9fa9520fb08ba3e48
Coolleak <sherbetlem@gmail.com>
2017-09-21 14:28:12 +0300

    Added tables "Bus", "BusType". Delete class myContext which was a copy of BusTicketContex

---
+++ BusTickets.WebAPI/BusTickets.DataAccess/Models/BusType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusTickets.DataAccess
+{
+    public class BusType
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Bus> Bus { get; set; }
+    }
+}

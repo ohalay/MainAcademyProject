3934e353c3450b685d134aa77545f1f9cd98b464
Coolleak <sherbetlem@gmail.com>
2017-09-26 20:48:34 +0300

    merge from DatabaseFeature 2

--- BusTickets.WebAPI/BusTickets.BusinessServices/Interfices/ICityService.cs
+++ BusTickets.WebAPI/BusTickets.BusinessServices/Interfices/ICityService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using BusTickets.DataAccess;
 
+namespace BusTickets.BusinessServices.Interfices
+{
     public interface ICityService
     {
         IList<CitiesNearby> GetCitiseNearby(int cityId);

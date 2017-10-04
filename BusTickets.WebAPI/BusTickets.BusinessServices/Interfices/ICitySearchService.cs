362b78f95251728a709c40b145efba4e8b625343
mykolanazaruk1992 <mykolanazaruk1992@gmail.com>
2017-10-04 11:10:43 +0300

    Controller, service, test which returns cities that coincide in  letters

---
+++ BusTickets.WebAPI/BusTickets.BusinessServices/Interfices/ICitySearchService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using BusTickets.DataAccess;
+
+namespace BusTickets.BusinessServices.Interfices
+{
+    public interface ICitySearchService
+    {
+        IList<City> GetCity(string startwithcity);
+    }
+}
---
+++ BusTickets.WebAPI/BusTickets.BusinessServices/Services/CitySearchService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BusTickets.BusinessServices.Interfices;
+using BusTickets.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusTickets.BusinessServices.Services
+{
+    public class CitySearchService : BaseService, ICitySearchService
+    {
+        public CitySearchService(IBusTicketDbContext context)
+            : base(context)
+        {
+        }
+
+        public IList<City> GetCity(string starwithcity)
+        {
+            {
+                return this.Context.Cities
+                    .AsNoTracking()
+                    .Where(c => c.Name.StartsWith(starwithcity))
+                    .ToList();
+            }
+        }
+    }
+}

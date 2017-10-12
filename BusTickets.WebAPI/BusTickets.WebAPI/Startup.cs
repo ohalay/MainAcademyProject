266e864f0e4c4cd7f400ab20c9e37bae887a668d
mykolanazaruk1992 <mykolanazaruk1992@gmail.com>
2017-10-12 13:43:38 +0300

    Implement Log4net Logging

--- BusTickets.WebAPI/BusTickets.WebAPI/Startup.cs
+++ BusTickets.WebAPI/BusTickets.WebAPI/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using BusTickets.BusinessServices.Interfices;
 using BusTickets.BusinessServices.Services;
 using BusTickets.DataAccess;
+using BusTickets.WebAPI.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -31,6 +32,7 @@ namespace BusTickets.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddLog4Net();
             app.UseMvc();
         }
     }

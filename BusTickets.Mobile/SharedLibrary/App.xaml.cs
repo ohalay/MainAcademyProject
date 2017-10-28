3d14d8f2946d366038eca35bec6bc5cb4c8493c8
mykolanazaruk1992 <mykolanazaruk1992@gmail.com>
2017-10-28 15:26:03 +0300

    Added Xamarin project

---
+++ BusTickets.Mobile/SharedLibrary/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SharedLibrary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new SharedLibrary.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("OnSleep");
+        }
+    }
+}
